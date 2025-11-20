3a302217b1c2e9a37e56fcdc9c1bbb89b96783aa
ADI_Anaya <anayaadrian121504@gmail.com>
2025-11-20 11:25:12 -0600

    client switch screens

--- src/main/java/MyController.java
+++ src/main/java/MyController.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -18,27 +19,56 @@ import javafx.scene.layout.VBox;
 //import javafx.scene.image.Image;
 
 public class MyController implements Initializable{
+    @FXML
+    public BorderPane borderPane;
 
     @FXML
-    BorderPane borderPane;
+    public TextField textFIP;
+    @FXML
+    public TextField textFPortNum;
 
+    @FXML
+    public Button connectToServer;
 
+    Client clientConnection;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
 
     public void connectToServerMethod() throws IOException{
+
+        String IP = textFIP.getText();
+        String portStr = textFPortNum.getText();
+        if(IP.isEmpty() || portStr.isEmpty()){
+            System.out.println("NO port or IP");
+            return;
+        }
+
+        int port;
+        try{
+            port = Integer.parseInt(portStr);
+            if(port < 1 || port > 65535){
+                System.out.println("Invalid port");
+                return;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Invalid port or IP");
+            return;
+        }
+
+        clientConnection = new Client(data ->{
+            Platform.runLater(()->{
+                textFPortNum.getText();
+            });
+        }
+        );
+        clientConnection.start();
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/game.fxml"));
         Parent root = loader.load();
         MyController controller = loader.getController();
-
-//        controller.textField1.clear();
-//        controller.textField2.setText("final string goes here");
-//        controller.textField2.clear();
-//        controller.but1.setDisable(false);
-//        controller.but1.setText("button one");
-
         borderPane.getScene().setRoot(root);
     }
 //    public void StopServerMethod(){

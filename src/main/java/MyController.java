fc27ff02f41acba3b3d4f12fb9e7c15ecd056564
AlexV68 <switch36e@gmail.com>
2025-11-22 23:48:00 -0600

    all 3 scenes(has everything - looks meh)

--- src/main/java/MyController.java
+++ src/main/java/MyController.java
@@ -1,27 +1,24 @@
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
 import javafx.scene.control.Alert;
 //import javafx.scene.image.Image;
 
 public class MyController implements Initializable{
 
     @FXML
-    BorderPane borderPane;
+    BorderPane welcomePane;
+
+    @FXML
+    BorderPane gamePane;
 
 
 
@@ -61,23 +58,30 @@ public class MyController implements Initializable{
 //    }
     @FXML
     public void startGame() throws IOException{
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/game.fxml"));
         Parent root = loader.load();
         MyController controller = loader.getController();
-
-
-        borderPane.getScene().setRoot(root);
+        welcomePane.getScene().setRoot(root);
     }
 
     @FXML
     public void playHand() throws IOException{
         System.out.println("Playing Hand");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/result.fxml"));
+        Parent root = loader.load();
+        MyController controller = loader.getController();
+        gamePane.getScene().setRoot(root);
     }
 
     @FXML
     public void fold() throws IOException{
         System.out.println("Folding");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/result.fxml"));
+        Parent root = loader.load();
+        MyController controller = loader.getController();
+        gamePane.getScene().setRoot(root);
     }
 
     @FXML
@@ -89,6 +93,12 @@ public class MyController implements Initializable{
     public void pairPlus() throws IOException{
         System.out.println("Pair plus");
     }
+
+    @FXML
+    public void playAgain() throws IOException{
+        System.out.println("Playing Again");
+    }
+
 
     //MENU
     @FXML
